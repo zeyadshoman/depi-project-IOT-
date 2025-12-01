9b5181e0be3a1aa6d33778d574f84c95187ebc07
Zeyad Shoman <zeyadshoman@gmail.com>
2025-12-01 02:25:25 +0200

    Update SQLQuery1.sql

--- real-time-iot-pipeline/src/SQLQuery1.sql
+++ real-time-iot-pipeline/src/SQLQuery1.sql
@@ -1,4 +1,21 @@
-create database DepiProject_cr7
-use DepiProject_cr7
-select * from dbo.iot_Streaming
+CREATE TABLE WeatherStation_Pro (
+    Timestamp datetime,
+    DeviceID nvarchar(100),
+    DeviceType nvarchar(100),
+    Location nvarchar(100),
+    Zone nvarchar(100),
+    Temperature_C float,
+    Humidity_pct float,
+    WindSpeed_kmh float,
+    WindDirection nvarchar(10),
+    Rainfall_mm float,
+    CloudCoverage_pct float,
+    UV_Index float,
+    Pressure_hPa float,
+    Battery_pct float,
+    AlertLevel int,
+    AlertType nvarchar(100),
+    Advisory nvarchar(300)
+);
+Select * From WeatherStation_Pro
 
